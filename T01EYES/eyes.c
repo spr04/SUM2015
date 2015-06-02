819da19d484d6b444b1ead31f65da91bd721b1a6
Ann Mitina <annmitina@gmail.com>
2015-06-02 12:17:41 +0400

    first

---
+++ T01EYES/eyes.c
@@ -0,0 +1,129 @@
+/* T01FWIN.C
+ * Пример базового макета программы под WinAPI.
+ * Создание и поддержка простейшего окна.
+ */
+
+#include <windows.h>
+
+/* Имя класса окна */
+#define WND_CLASS_NAME "My window class"
+
+/* Ссылка вперед */
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam );
+
+/* Главная функция программы.
+ *   - дескриптор экземпляра приложения:
+ *       HINSTANCE hInstance;
+ *   - дескриптор предыдущего экземпляра приложения
+ *     (не используется и должно быть NULL):
+ *       HINSTANCE hPrevInstance;
+ *   - командная строка:
+ *       CHAR *CmdLine;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (INT) код возврата в операционную систему.
+ *   0 - при успехе.
+ */
+INT WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    CHAR *CmdLine, INT ShowCmd )
+{
+  WNDCLASS wc;
+  HWND hWnd;
+  MSG msg;
+  /* HINSTANCE hIns = LoadLibrary("shell32"); */
+
+  /* Регистрация класса окна */
+  wc.style = CS_VREDRAW | CS_HREDRAW; /* Стиль окна: полностью перерисовывать
+                                       * при изменении вертикального или
+                                       * горизонтального размеров
+                                       * еще можно CS_DBLCLKS для добавления
+                                       * отработки двойного нажатия */
+  wc.cbClsExtra = 0; /* Дополнительное количество байт для класса */
+  wc.cbWndExtra = 0; /* Дополнительное количество байт для окна */
+  wc.hbrBackground = CreateSolidBrush(RGB(255, 255, 0));
+  wc.hCursor = LoadCursor(NULL, IDC_HAND); /* Загрузка курсора (системного) */
+  wc.hIcon = LoadIcon(NULL, IDI_ASTERISK); /* Загрузка пиктограммы (системной) */
+  wc.hInstance = hInstance; /* Дескриптор приложения, регистрирующего класс */
+  wc.lpszMenuName = NULL; /* Имя ресурса меню */
+  wc.lpfnWndProc = MyWindowFunc; /* Указатель на функцию обработки */
+  wc.lpszClassName = WND_CLASS_NAME;
+
+  /* Регистрация класса в системе */
+  if (!RegisterClass(&wc))
+  {
+    MessageBox(NULL, "Error register window class", "ERROR", MB_OK);
+    return 0;
+  }
+
+  /* Создание окна */
+  hWnd =
+    CreateWindow(WND_CLASS_NAME,    /* Имя класса окна */
+      "Pie",                      /* Заголовок окна */
+      WS_OVERLAPPEDWINDOW,          /* Стили окна - окно общего вида */
+      CW_USEDEFAULT, CW_USEDEFAULT, /* Позиция окна (x, y) - по умолчанию */
+      CW_USEDEFAULT, CW_USEDEFAULT, /* Размеры окна (w, h) - по умолчанию */
+      NULL,                         /* Дескриптор родительского окна */
+      NULL,                         /* Дескриптор загруженного меню */
+      hInstance,                    /* Дескриптор приложения */
+      NULL);                        /* Указатель на дополнительные параметры */
+
+  ShowWindow(hWnd, ShowCmd);
+  UpdateWindow(hWnd);
+
+  /* Запуск цикла сообщений окна */
+  while (GetMessage(&msg, NULL, 0, 0))
+    /* Передача сообщений в функцию окна */
+    DispatchMessage(&msg);
+
+  return 30;
+} /* End of 'WinMain' function */
+
+/* Функция обработки сообщения окна.
+ * АРГУМЕНТЫ:
+ *   - дескриптор окна:
+ *       HWND hWnd;
+ *   - номер сообщения (см. WM_***):
+ *       UINT Msg;
+ *   - параметр сообшения ('word parameter'):
+ *       WPARAM wParam;
+ *   - параметр сообшения ('long parameter'):
+ *       LPARAM lParam;
+ * ВОЗВРАЩАЕМОЕ ЗНАЧЕНИЕ:
+ *   (LRESULT) - в зависимости от сообщения.
+ */
+LRESULT CALLBACK MyWindowFunc( HWND hWnd, UINT Msg,
+                               WPARAM wParam, LPARAM lParam )
+{
+  POINT pt;
+  HDC hDC;
+  static INT w, h;
+
+  switch (Msg)
+  {
+  case WM_CREATE:
+    SetTimer(hWnd, 111, 50, NULL);
+    return 0;
+  case WM_SIZE:
+    w = LOWORD(lParam);
+    h = HIWORD(lParam);
+    return 0;
+  case WM_TIMER:
+    hDC = GetDC(hWnd);
+    Rectangle(hDC, 0, 0, w, h) ;
+    Ellipse(hDC, w / 2 + 50, 50 , w - 50, h - 50);
+    Ellipse(hDC, 50, 50, w / 2 - 50, h - 50);
+    GetCursorPos(&pt);
+    ScreenToClient(hWnd, &pt);
+    
+    Ellipse(hDC, , , , );
+   
+    ReleaseDC(hWnd, hDC);
+    return 0;
+  case WM_DESTROY:
+    KillTimer(hWnd, 111);
+    PostMessage(hWnd, WM_QUIT, 0, 0);
+    return 0;
+  }
+  return DefWindowProc(hWnd, Msg, wParam, lParam);
+} /* End of 'MyWindowFunc' function */
+/* END OF 'T01FWIN.C' FILE */
