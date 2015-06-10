a5f04b52a3712d904c599c575a823ef1c1d03f41
Ann Mitina <annmitina@gmail.com>
2015-06-10 11:10:02 +0400

    the same

--- T06ANIM/units.h
+++ T06ANIM/units.h
@@ -1,3 +1,13 @@
+/* FILENAME: UNITS.C
+ * PROGRAMMER: AM1
+ * PURPOSE: linking for files.
+ * LAST UPDATE: 09.06.2015
+ */
+
 #include <stdio.h>
 
 am1UNIT * AM1_UnitBallCreate( VOID );
+VOID ObjDraw( HDC hDC, INT H, INT W );
+BOOL ObjLoad( CHAR *FileName );
+
+/* END OF 'UNITS.H' FILE */
